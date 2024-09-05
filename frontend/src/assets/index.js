4e080be1a03140bd996e4350b6c7456f49c97c0c
HawraaYounes <hawraayounes0@gmail.com>
2024-09-05 13:39:37 +0300

    added Navbar Component

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,6 @@
+import userAlertIcon from "./icons/mdi_account-alert-outline.svg";
+import searchIcon from "./icons/akar-icons_search.svg";
+import favIcon from "./icons/akar-icons_heart.svg";
+import cartIcon from "./icons/ant-design_shopping-cart-outlined.svg";
+
+export { userAlertIcon, searchIcon, favIcon, cartIcon };
---
+++ frontend/src/constants/index.js
@@ -0,0 +1,37 @@
+import { userAlertIcon, searchIcon, favIcon, cartIcon } from "../assets";
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "shop",
+    title: "Shop",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+export const navIcons = [
+  {
+    id: "user",
+    icon: userAlertIcon,
+  },
+  {
+    id: "search",
+    icon: searchIcon,
+  },
+  {
+    id: "fav",
+    icon: favIcon,
+  },
+  {
+    id: "cart",
+    icon: cartIcon,
+  },
+];
