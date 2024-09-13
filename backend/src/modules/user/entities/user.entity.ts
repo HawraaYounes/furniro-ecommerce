a70362e6892748af3dd3917973da2bbacfc835ee
HawraaYounes <hawraayounes0@gmail.com>
2024-09-13 16:32:18 +0300

    added roles column to user entity

--- backend/src/modules/user/entities/user.entity.ts
+++ backend/src/modules/user/entities/user.entity.ts
@@ -1,23 +1,27 @@
 import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import { Role } from '../../enums/roles.enum'; // Import the Role enum
 
 @Entity()
 @Unique(["email"])
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column({ type: 'varchar', length: 30 })
+  name: string;
 
-    @Column({ type: 'varchar', length: 30 })
-    name: string;
+  @Column({ type: 'varchar', length: 15 })
+  username: string;
 
-    @Column({ type: 'varchar', length: 15 })
-    username: string;
+  @Column({ type: 'varchar', length: 40 })
+  email: string;
 
-    @Column({ type: 'varchar', length: 40 })
-    email: string;
+  @Column({ type: 'varchar' })
+  password: string;
 
-    @Column({ type: 'varchar' })
-    password: string;
-
+  @Column({
+    type: 'simple-array', 
+    default: [Role.User] 
+  })
+  roles: Role[];
 }
-
