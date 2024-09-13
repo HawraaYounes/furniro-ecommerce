9f13c7317183c86e85ab29210abfa8b720dde60e
HawraaYounes <hawraayounes0@gmail.com>
2024-09-13 19:19:57 +0300

    added user roles to payload

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService {
     if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
-    const payload = { sub: user.id, email: user.email };
+    const payload = { sub: user.id, email: user.email, roles: user.roles };
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
