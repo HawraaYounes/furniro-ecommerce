76008c9e772b348363980c68700b4bd1f2ce4837
HawraaYounes <hawraayounes0@gmail.com>
2024-09-14 11:51:27 +0300

    added login page and form

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import Nav from "../components/Nav"
+import LoginForm from "../sections/login/LoginForm"
+
+const Login = () => {
+  return (
+    <>
+      <Nav/>
+      <LoginForm/>
+    </>
+  )
+}
+
+export default Login
---
+++ frontend/src/sections/login/LoginForm.jsx
@@ -0,0 +1,11 @@
+import styles from "../../style"
+
+const LoginForm = () => {
+  return (
+    <section className="">
+        <h2 className={`${styles.heading}`}>Login</h2>
+    </section>
+  )
+}
+
+export default LoginForm
