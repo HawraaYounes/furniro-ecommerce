761f84b550bfe1f3b303dc5c6632425cb56fcda9
HawraaYounes <hawraayounes0@gmail.com>
2024-09-14 15:43:13 +0300

    added button component

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({label}) => {
+  return (
+    <button className='m-1 bg-accent py-3 px-8 rounded-lg text-white'>{label}</button>
+  )
+}
+
+export default Button
