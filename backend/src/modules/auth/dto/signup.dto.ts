789dd043c870ee3dcdba88b2b197c46359014db3
HawraaYounes <hawraayounes0@gmail.com>
2024-09-15 14:02:55 +0300

    added signin and signup dto

---
+++ backend/src/modules/auth/dto/signup.dto.ts
@@ -0,0 +1,39 @@
+import {
+    IsEmail,
+    IsEnum,
+    IsNotEmpty,
+    IsOptional,
+    IsString,
+    Matches,
+    MinLength,
+  } from 'class-validator';
+  import { Role } from 'src/modules/enums/roles.enum';
+
+  const passwordRegEx =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
+
+  
+  export class SignUpDto {
+    @IsString()
+    @MinLength(2, { message: 'Name must have at least 2 characters.' })
+    @IsNotEmpty()
+    name: string;
+  
+    @IsNotEmpty()
+    @IsEmail({}, { message: 'Please provide valid Email.' })
+    email: string;
+  
+    @IsNotEmpty()
+    @Matches(passwordRegEx, {
+      message: `Password must contain Minimum 8 and maximum 20 characters, 
+      at least one uppercase letter, 
+      one lowercase letter, 
+      one number and 
+      one special character`,
+    })
+    password: string;
+
+    @IsOptional()
+    @IsEnum(Role, { each: true, message: 'Invalid role provided.' })
+    roles?: Role[] ; 
+  }
