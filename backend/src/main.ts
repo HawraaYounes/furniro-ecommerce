2997d99dedc45a557eef01c8a90704bb9b4500ba
HawraaYounes <hawraayounes0@gmail.com>
2024-09-15 14:03:12 +0300

    enabled validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-    // Enable CORS with specific options
-    app.enableCors({
-      origin: 'http://localhost:5173', 
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      allowedHeaders: 'Content-Type, Authorization',
-    });
+  // Enable CORS with specific options
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  // Enable global validation pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, 
+      forbidNonWhitelisted: true, 
+      transform: true, 
+    }),
+  );
 
   await app.listen(3000);
 }
