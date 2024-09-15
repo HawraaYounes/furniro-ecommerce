69202fa195b281ca456ee7e75758c0c180aded5a
HawraaYounes <hawraayounes0@gmail.com>
2024-09-15 09:58:18 +0300

    updated input component

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Input = ({ label }) => {
+const Input = ({ label , name, type="text"}) => {
   return (
     <div>
-      <label>{label}</label>
-      <input type="text" className="border border-black m-1"/>
+       <label htmlFor={name}>{label}</label>
+       <input id={name} name={name} type={type} className="border-black m-1 rounded-lg" />
     </div>
   );
 };
