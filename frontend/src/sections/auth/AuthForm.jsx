e1496b64e5c61ab97dd38c281536ea201ff120f1
HawraaYounes <hawraayounes0@gmail.com>
2024-09-15 09:58:30 +0300

    updated auth form

--- frontend/src/sections/auth/AuthForm.jsx
+++ frontend/src/sections/auth/AuthForm.jsx
@@ -1,23 +1,28 @@
-import { Link, useSearchParams } from "react-router-dom";
+import { Form, Link, useSearchParams } from "react-router-dom";
 import Input from "../../components/Input";
 import styles from "../../style";
 import Button from "../../components/Button";
+
 
 const LoginForm = () => {
   const [searchParams] = useSearchParams();
   const isLogin = searchParams.get("mode") === "login";
 
   return (
-    <section className="">
-      <h2 className={`${styles.heading}`}>{isLogin ? 'Log In' : "Create New Account"}</h2>
+    <Form className="" method="post">
+      <h2 className={`${styles.heading}`}>
+        {isLogin ? "Log In" : "Create New Account"}
+      </h2>
       <Input label="Email" />
       <Input label="Password" />
-      {!isLogin && <Input label="Confirm Password"/>}
-      <Button label="Save"/>
+      {!isLogin && <Input label="Confirm Password" />}
+      <Button label="Save" type="submit" />
       <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
-        {isLogin ? "New Customer? Create your account" : "Already have an account? Login Here"}
+        {isLogin
+          ? "New Customer? Create your account"
+          : "Already have an account? Login Here"}
       </Link>
-    </section>
+    </Form>
   );
 };
 
