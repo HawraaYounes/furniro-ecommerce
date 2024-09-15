d07d652e5d73f79d0129a9647da260c7ae1d347e
HawraaYounes <hawraayounes0@gmail.com>
2024-09-15 11:18:15 +0300

    allow cors in backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    // Enable CORS with specific options
+    app.enableCors({
+      origin: 'http://localhost:5173', // Replace with your frontend URL
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'Content-Type, Authorization',
+    });
+    
   await app.listen(3000);
 }
 bootstrap();
