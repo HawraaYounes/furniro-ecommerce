4ae61689199e716680418c91546612f1136fca1f
HawraaYounes <hawraayounes0@gmail.com>
2024-09-17 17:43:46 +0300

    added tags

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -26,15 +26,16 @@ async function bootstrap() {
     .setTitle('Furniro Furniture API')
     .setDescription('API documentation for the Furniro e-commerce platform')
     .setVersion('1.0')
-    .addTag('furniture')
+    .addTag('auth')
+    .addTag('user')
     .build();
-    const options: SwaggerDocumentOptions =  {
-      operationIdFactory: (
-        controllerKey: string,
-        methodKey: string
-      ) => methodKey
-    };
-    
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (
+      controllerKey: string,
+      methodKey: string
+    ) => methodKey
+  };
+
   const document = SwaggerModule.createDocument(app, config, options);
   SwaggerModule.setup('api', app, document);
 
