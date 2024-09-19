8d93d0268d1a1cc52c14d04c1da888032e5c35ce
HawraaYounes <hawraayounes0@gmail.com>
2024-09-19 12:04:54 +0300

    added example response

---
+++ backend/src/constants/responses/en/user/user-created.ts
@@ -0,0 +1,8 @@
+import { STATUS_CREATED } from "src/constants/codes/status-codes";
+
+export const USER_CREATED = {
+  success: true,
+  statusCode: STATUS_CREATED, 
+  message: 'User successfully created',
+  data: null,
+};
