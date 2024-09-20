7386cb5596534733ea420566d27472783cc591f7
HawraaYounes <hawraayounes0@gmail.com>
2024-09-20 13:18:15 +0300

    added auth routes responses

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -3,21 +3,48 @@ import { AuthService } from "./auth.service";
 import { Public } from "./public-strategy";
 import { SignInDto } from "./dto/signin.dto";
 import { SignUpDto } from "./dto/signup.dto";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiBadRequestResponse, ApiBody, ApiConflictResponse, ApiCreatedResponse, ApiInternalServerErrorResponse, ApiNotFoundResponse, ApiOkResponse, ApiTags, ApiUnauthorizedResponse } from "@nestjs/swagger";
 
 @ApiTags('auth')
 @Controller("auth")
 export class AuthController {
     constructor(private authService: AuthService) { }
     @Public()
-    @HttpCode(HttpStatus.OK)
+    @ApiBody({ type: SignInDto })
+    @ApiOkResponse({
+        description: 'User successfully logged in.',
+    })
+    @ApiUnauthorizedResponse({
+        description: 'Invalid email or password.',
+    })
+    @ApiNotFoundResponse({
+        description: 'User not found.',
+    })
+    @ApiBadRequestResponse({
+        description: 'Validation errors or bad request.',
+    })
+    @ApiInternalServerErrorResponse({
+        description: 'Unexpected error occurred during sign-in.',
+    })
     @Post("login")
     signIn(@Body() body: SignInDto) {
         return this.authService.signIn(body);
     }
 
     @Public()
-    @HttpCode(HttpStatus.OK)
+    @ApiBody({ type: SignUpDto })
+    @ApiCreatedResponse({
+        description: 'User successfully Signed Up.',
+    })
+    @ApiConflictResponse({
+        description: 'When the email is already taken.',
+    })
+    @ApiBadRequestResponse({
+        description: 'Validation errors or bad request.',
+    })
+    @ApiInternalServerErrorResponse({
+        description: 'Unexpected error occurred while processing the signup.',
+    })
     @Post("signup")
     signUp(@Body() body: SignUpDto) {
         return this.authService.signUp(body);
