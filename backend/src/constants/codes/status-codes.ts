f04f04201c9e1ec0c6a12590c150d497e0aebb1b
HawraaYounes <hawraayounes0@gmail.com>
2024-09-20 11:08:02 +0300

    added email registered response

--- backend/src/constants/codes/status-codes.ts
+++ backend/src/constants/codes/status-codes.ts
@@ -3,6 +3,7 @@ export const STATUS_OK = 200;
 export const STATUS_CREATED = 201;
 
 //ERROR CODES
-export const STATUS_UNAUTHORIZED= 401; 
+export const STATUS_UNAUTHORIZED = 401;
 export const STATUS_NOT_FOUND = 404;
+export const STATUS_CONFLICT = 409;
 export const STATUS_INTERNAL_SERVER_ERROR = 500;
