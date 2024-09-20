0229f5852c859cc3d0b48498637da3903ae1c259
HawraaYounes <hawraayounes0@gmail.com>
2024-09-20 11:08:12 +0300

    added signup success response

---
+++ backend/src/constants/responses/en/auth/signup-success.ts
@@ -0,0 +1,8 @@
+import { STATUS_CREATED } from "src/constants/codes/status-codes";
+
+export const SIGNUP_SUCCESS = {
+    success: true,
+    statusCode: STATUS_CREATED,
+    message: 'User successfully signed up',
+    data: null,
+  };
