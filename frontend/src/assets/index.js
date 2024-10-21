fa4810c33363d4835dccf77d292c2b7dbff1c498
HawraaYounes <hawraayounes0@gmail.com>
2024-10-21 13:41:24 +0300

    added icons

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -2,6 +2,10 @@ import userAlertIcon from "./icons/mdi_account-alert-outline.svg";
 import searchIcon from "./icons/akar-icons_search.svg";
 import favIcon from "./icons/akar-icons_heart.svg";
 import cartIcon from "./icons/ant-design_shopping-cart-outlined.svg";
+import questionIcon from "./icons/question.svg"
+import successIcon from "./icons/success.svg"
+import warningIcon from "./icons/warning.svg"
+import failIcon from "./icons/fail.svg"
 
 import bedroomImg from "./images/bedroom.png";
 import livingImg from "./images/living.png";
@@ -12,6 +16,10 @@ export {
   searchIcon,
   favIcon,
   cartIcon,
+  questionIcon,
+  successIcon,
+  warningIcon,
+  failIcon,
   bedroomImg,
   livingImg,
   diningImg,
