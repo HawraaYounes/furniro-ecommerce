b2b24cc480af9cdb4094aace615c28152e549d2b
HawraaYounes <hawraayounes0@gmail.com>
2024-10-22 13:54:51 +0300

    Adjusted walert component

--- frontend/src/components/Alert.jsx
+++ frontend/src/components/Alert.jsx
@@ -44,28 +44,28 @@ const Alert = ({ type, message, description, duration = 5000 }) => {
 
   return (
     <div
-      className={`relative flex font-poppins w-1/2 rounded-[32px] ${bgColor} text-white shadow-md max-w-[600px] mx-5 justify-between items-center`}
+      className={`relative flex font-poppins w-1/2 rounded-[32px] ${bgColor} text-white shadow-md max-w-[600px] mx-5 justify-between items-center py-4 pl-4`}
     >
       {/* Icon container */}
       <div className="mb-3">
         <img
           src={icon}
           alt={`${type} icon`}
-          className="w-[78px] h-[78px] absolute top-[-40px]"
+          className="w-[70px] h-[70px] absolute top-[-40px] "
         />
       </div>
       {/* Bubbles image */}
-      <div className="absolute bottom-0 left-0">
-        <img
-          src={redBubbles}
-          alt="bubbles"
-          className="rounded-[32px]"
-        />
+      <div className="absolute bottom-0 left-0 ">
+        <img src={redBubbles} alt="bubbles" className="rounded-[32px]" />
       </div>
       {/* Text container */}
-      <div className="text-left py-4 px-6 ml-[90px]"> {/* Adjusted margin to make space for the icon */}
+      <div className="text-left py-4 px-6 ml-[90px]">
+        {" "}
+        {/* Adjusted margin to make space for the icon */}
         <p className="text-[24px]">{message}</p>
-        {description && <p className="text-[14px] font-light max-w-">{description}</p>}
+        {description && (
+          <p className="text-[14px] font-light max-w-">{description}</p>
+        )}
       </div>
     </div>
   );
