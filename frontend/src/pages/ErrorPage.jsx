61314a42e6bb4aa253ddac79f91bb6bfa5eeaf81
HawraaYounes <hawraayounes0@gmail.com>
2024-10-25 17:27:45 +0300

    styled error page

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,22 +1,21 @@
 import { Link } from "react-router-dom";
 import Nav from "../components/Nav";
+import Button from "../components/Button";
 
 const ErrorPage = () => {
   return (
     <>
     <Nav/>
     <div className="flex flex-col justify-center items-center min-h-screen bg-white">
-      <h1 className="text-6xl font-serif text-black mb-4">PAGE NOT FOUND</h1>
-      <p className="text-lg text-gray-600 mb-8">
+      <h1 className="text-3xl md:text-6xl font-abhaya font-semibold text-black mb-4">PAGE NOT FOUND</h1>
+      <p className=" md:text-xl text-gray-600 mb-8 font-poppins w-1/2">
         The page you were looking for could not be found. It might have been
         removed, renamed, or did not exist in the first place.
       </p>
-      <Link
-        to="/"
-        className="bg-black text-white px-6 py-3 font-medium"
-      >
-        GO TO HOME PAGE
-      </Link>
+      
+      <Link to="/">
+          <Button label="GO TO HOME PAGE" /> 
+        </Link>
     </div></>
   );
 };
