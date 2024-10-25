7b67365a7d4bf7a48ed44ef9ccd01b55d3847f50
HawraaYounes <hawraayounes0@gmail.com>
2024-10-25 17:31:34 +0300

    changed button font weight

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({label}) => {
   return (
-    <button className="font-poppins bg-[#B88E2F] text-white py-5 px-12 md:py-[25px] md:px-[72px] md:mt-10 mt-1 font-bold ">{label}</button>
+    <button className="font-poppins bg-[#B88E2F] text-white py-5 px-12 md:py-[25px] md:px-[72px] md:mt-10 mt-1 font-semibold ">{label}</button>
   )
 }
 
