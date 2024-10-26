d8a5416721f2c0ca5a64361937726756105b2d3e
HawraaYounes <hawraayounes0@gmail.com>
2024-10-26 17:58:49 +0300

    added product links

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+const products = [
+  {
+    id: 1,
+    title: "Modern Sofa",
+  },
+  {
+    id: 2,
+    title: "Wooden Dining Table",
+  },
+  {
+    id: 3,
+    title: "King Size Bed",
+  },
+];
 
 const Shop = () => {
   return (
     <div>
-        Shop Page
+      <h1>Shop Page</h1>
+      <ul>
+        {products.map((product) => (
+          <li>
+            <Link to={`/shop/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
