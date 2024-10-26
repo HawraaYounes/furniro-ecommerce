d264395381eb591c5a9b89cfe9c515f317dde1c4
HawraaYounes <hawraayounes0@gmail.com>
2024-10-26 17:34:32 +0300

    fixed logo path

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { navIcons, navLinks } from "../constants";
 const Nav = () => {
   return (
     <nav className="flex justify-between items-center px-10 md:px-[77px] py-6 bg-white">
-      <img src="logo.svg" />
+      <img src="/logo.svg" />
       <ul className="list-none hidden sm:flex flex-row gap-10 ">
         {navLinks.map((link) => (
           <li key={link.id} className="font-poppins text-black font-medium ">
