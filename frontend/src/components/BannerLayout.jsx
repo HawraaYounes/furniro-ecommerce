3f248d9c43b557ca3aee5489e67db2a3f0300822
HawraaYounes <hawraayounes0@gmail.com>
2024-10-29 15:46:07 +0200

    fixed duplicate backgrounds

--- frontend/src/components/BannerLayout.jsx
+++ frontend/src/components/BannerLayout.jsx
@@ -10,22 +10,22 @@ const CommonBannerLayout = () => {
     <>
       {bannerData.title && (
         <div
-          className="relative w-full h-56 bg-cover bg-center font-poppins"
+          className="h-56 bg-contain bg-no-repeat font-poppins"
           style={{
             backgroundImage: `url(${bannerData.backgroundImage})`,
           }}
         >
-          <div
-            className="absolute bg-cover bg-center"
-            style={{ backgroundImage: `url(${bannerData.backgroundImage})` }}
-          ></div>
           <div className="flex flex-col justify-center items-center h-full">
             <h1 className="text-5xl font-medium text-black mb-2">
               {bannerData.title}
             </h1>
             <p className="font-light">
               <span className="font-medium">
-                Home <img src={arrowRightIcon} className="inline w-5 h-5 font-thin" />
+                Home{" "}
+                <img
+                  src={arrowRightIcon}
+                  className="inline w-5 h-5 font-thin"
+                />
               </span>
               {bannerData.subtitle}
             </p>
