89a44b8001121d4998c9e20ab82a05a5f99b0e63
HawraaYounes <hawraayounes0@gmail.com>
2024-10-29 14:08:07 +0200

    added banner layout

--- frontend/src/components/BannerLayout.jsx
+++ frontend/src/components/BannerLayout.jsx
@@ -1,28 +1,39 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
+import { arrowRightIcon } from "../assets";
 
 const CommonBannerLayout = () => {
   const bannerData = useSelector((state) => state.banner);
 
   return (
-    <div>
+    <>
       {bannerData.title && (
         <div
-          className="banner"
+          className="relative w-full h-56 bg-cover bg-center font-poppins"
           style={{
             backgroundImage: `url(${bannerData.backgroundImage})`,
-            padding: "20px",
-            color: "white",
-            textAlign: "center",
           }}
         >
-          <h1>{bannerData.title}</h1>
-          <p>{bannerData.subtitle}</p>
+          <div
+            className="absolute bg-cover bg-center"
+            style={{ backgroundImage: `url(${bannerData.backgroundImage})` }}
+          ></div>
+          <div className="flex flex-col justify-center items-center h-full">
+            <h1 className="text-5xl font-medium text-black mb-2">
+              {bannerData.title}
+            </h1>
+            <p className="">
+              <span className="font-bold">
+                Home <img src={arrowRightIcon} className="inline w-5 h-5 font-thin" />
+              </span>
+              {bannerData.subtitle}
+            </p>
+          </div>
         </div>
       )}
       <Outlet />
-    </div>
+    </>
   );
 };
 
