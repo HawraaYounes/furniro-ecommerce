41e803e1fbf944a96f1dce63c3fc0a85cef3f9a0
HawraaYounes <hawraayounes0@gmail.com>
2024-11-02 15:47:28 +0200

    updated product card

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,9 +1,14 @@
-const ProductCard = ({ image }) => {
+import { useLoaderData } from "react-router-dom";
+
+const ProductCard = ({ product }) => {
+  
   return (
     <div>
       <div
-        className={`bg-[url('${image}')] bg-cover bg-center h-72 w-48 rounded-lg`}
-      ></div>
+        className={`bg-[url('${product.thumbnail}')] bg-cover bg-center h-72 w-48 rounded-lg`}
+      >
+        <p>{product.title}</p>
+      </div>
     </div>
   );
 };
