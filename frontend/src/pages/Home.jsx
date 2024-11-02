20d94896ddf95042d1438a11e86795ba16822cfb
HawraaYounes <hawraayounes0@gmail.com>
2024-11-02 15:42:53 +0200

    fetched products in home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import Hero from "../sections/home/Hero";
 import Categories from "../sections/home/Categories";
-import Products from "../sections/home/Products";
+import { Link, useLoaderData } from "react-router-dom";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
+  const data = useLoaderData();
+  console.log("PRODUCTS IN HOME", data.products);
   return (
     <>
       <div className=" bg-hero-pattern bg-no-repeat bg-cover bg-center ">
         <Hero />
       </div>
       <Categories />
-      <Products />
+      <ul>
+        {data.products.map((product) => (
+          <li key={product.id}>
+            <Link to={`/shop/${product.id}`}>
+              <ProductCard product={product} />
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
 
+export const fetchProductsLoader = async () => {
+  const response = await fetch(
+    "https://dummyjson.com/products?limit=10&skip=10&select=title,price"
+  );
+  if (!response.ok) {
+    console.log("failed");
+  } else {
+    return response;
+  }
+};
 export default Home;
