f867262174980dc2d10c4d6b80a89a03ad4f21e0
HawraaYounes <hawraayounes0@gmail.com>
2024-11-03 13:39:15 +0200

    added chair asset

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -2,13 +2,12 @@ import userAlertIcon from "./icons/mdi_account-alert-outline.svg";
 import searchIcon from "./icons/akar-icons_search.svg";
 import favIcon from "./icons/akar-icons_heart.svg";
 import cartIcon from "./icons/ant-design_shopping-cart-outlined.svg";
-import questionIcon from "./icons/question.svg"
-import successIcon from "./icons/success.svg"
-import warningIcon from "./icons/warning.svg"
-import failIcon from "./icons/fail.svg"
-import closeIcon from "./icons/close.svg"
-import arrowRightIcon from "./icons/arrow-right.svg"
-
+import questionIcon from "./icons/question.svg";
+import successIcon from "./icons/success.svg";
+import warningIcon from "./icons/warning.svg";
+import failIcon from "./icons/fail.svg";
+import closeIcon from "./icons/close.svg";
+import arrowRightIcon from "./icons/arrow-right.svg";
 
 import bedroomImg from "./images/bedroom.png";
 import livingImg from "./images/living.png";
@@ -18,7 +17,7 @@ import redBubbles from "./images/red-bubbles.png";
 import grayBubbles from "./images/gray-bubbles.png";
 import yellowBubbles from "./images/yellow-bubbles.png";
 import banner from "./images/banner.png";
-
+import chair from "./images/chair.png";
 
 export {
   userAlertIcon,
@@ -38,5 +37,6 @@ export {
   bedroomImg,
   livingImg,
   diningImg,
+  chair,
   banner,
 };
