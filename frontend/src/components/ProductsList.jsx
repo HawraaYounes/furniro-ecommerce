3fb2712f222052624f4c8dad8c9bca72b6a0d9ea
HawraaYounes <hawraayounes0@gmail.com>
2024-11-03 13:39:54 +0200

    updated product list style

--- frontend/src/components/ProductsList.jsx
+++ frontend/src/components/ProductsList.jsx
@@ -1,13 +1,14 @@
+// src/components/ProductList.js
 import React from "react";
 import { Link } from "react-router-dom";
 import ProductCard from "./ProductCard";
 
 const ProductList = ({ products }) => {
   return (
-    <div>
-      <ul>
+    <div className="p-4">
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {products.map((product) => (
-          <li key={product.id}>
+          <li key={product.id} className="overflow-hidden bg-white">
             <Link to={`/shop/${product.id}`}>
               <ProductCard product={product} />
             </Link>
