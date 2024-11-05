68e22f3cc374f991aeb78907d7fecd121e7ce995
HawraaYounes <hawraayounes0@gmail.com>
2024-11-05 17:16:29 +0200

    added create category custom responses

---
+++ backend/src/constants/responses/en/category/category-created.ts
@@ -0,0 +1,8 @@
+import { STATUS_CREATED } from "src/constants/codes/status-codes";
+
+export const CATEGORY_CREATED = {
+  success: true,
+  statusCode: STATUS_CREATED, 
+  message: 'Category successfully created',
+  data: null,
+};
---
+++ backend/src/constants/responses/en/common/internal-server-error.ts
@@ -0,0 +1,8 @@
+import { STATUS_INTERNAL_SERVER_ERROR } from "src/constants/codes/status-codes";
+
+export const INTERNAL_SERVER_ERROR = {
+    success: false,
+    statusCode: STATUS_INTERNAL_SERVER_ERROR,
+    message: 'Internal Server Error!',
+    data: null,
+  };
