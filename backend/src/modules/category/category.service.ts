2928924c8fcb6c9985a403660673a8489c62d013
HawraaYounes <hawraayounes0@gmail.com>
2024-11-05 12:05:08 +0200

    added category service

---
+++ backend/src/modules/category/category.service.ts
@@ -0,0 +1,37 @@
+// src/category/category.service.ts
+
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from './category.entity';
+import { CreateCategoryDto } from './dto/create-category.dto';
+
+@Injectable()
+export class CategoryService {
+    constructor(
+        @InjectRepository(Category)
+        private categoryRepository: Repository<Category>,
+    ) {}
+
+    create(payload:CreateCategoryDto): Promise<Category> {
+        const category = this.categoryRepository.create(payload);
+        return this.categoryRepository.save(category);
+    }
+
+    findAll(): Promise<Category[]> {
+        return this.categoryRepository.find();
+    }
+
+    findOne(id: number): Promise<Category> {
+        return this.categoryRepository.findOne({ where: { id } });
+    }
+
+    async update(id: number, name: string, description?: string): Promise<Category> {
+        await this.categoryRepository.update(id, { name, description });
+        return this.findOne(id);
+    }
+
+    async remove(id: number): Promise<void> {
+        await this.categoryRepository.delete(id);
+    }
+}
