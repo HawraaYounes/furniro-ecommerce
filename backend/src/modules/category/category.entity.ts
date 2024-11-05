fc5f657a08f4fc3e2a32521ac20d109f3876ab24
HawraaYounes <hawraayounes0@gmail.com>
2024-11-05 11:57:21 +0200

    added category entity

---
+++ backend/src/modules/category/category.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+
+@Entity('categories')
+export class Category {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    name: string;
+
+    @Column({ nullable: true })
+    description: string;
+
+  
+}
