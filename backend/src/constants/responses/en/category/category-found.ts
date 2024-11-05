55e3fb36afb2119ed2cde384687a63ac25eff36a
HawraaYounes <hawraayounes0@gmail.com>
2024-11-05 17:38:37 +0200

    added find one category custom responses

---
+++ backend/src/constants/responses/en/category/category-found.ts
@@ -0,0 +1,8 @@
+import { STATUS_OK } from "src/constants/codes/status-codes";
+
+export const CATEGORY_FOUND = {
+    success: true,
+    statusCode: STATUS_OK,
+    message: 'Category found successfully',
+    data: null,
+  };
---
+++ backend/src/constants/responses/en/category/category-not-found.ts
@@ -0,0 +1,8 @@
+import { STATUS_NOT_FOUND } from "src/constants/codes/status-codes";
+
+export const CATEGORY_NOT_FOUND = {
+    success: false,
+    statusCode: STATUS_NOT_FOUND,
+    message: 'Category not found',
+    data: null,
+  };
