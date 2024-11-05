7f267449611d741a6d4483b36da640cbf5ab2b6c
HawraaYounes <hawraayounes0@gmail.com>
2024-11-05 16:30:46 +0200

    updated get all categories

--- backend/src/modules/category/category.service.ts
+++ backend/src/modules/category/category.service.ts
@@ -1,6 +1,6 @@
 // src/category/category.service.ts
 
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Category } from './category.entity';
@@ -9,6 +9,7 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 import { FindCategoryParamsDto } from './dto/get-category.dto';
 import { DeleteCategoryParamsDto } from './dto/delete-category.dto';
 import { CATEGORIES_RETRIEVED } from 'src/constants/responses/en/category/categories-retrieved';
+import { STATUS_INTERNAL_SERVER_ERROR } from 'src/constants/codes/status-codes';
 
 @Injectable()
 export class CategoryService {
@@ -23,10 +24,26 @@ export class CategoryService {
     }
 
     async findAll() {
-        const categories = await this.categoryRepository.find();
-        return {
-            ...CATEGORIES_RETRIEVED,
-            data: categories
+        try {
+            const categories = await this.categoryRepository.find();
+            // Case 1: Check if no categories are found
+            if (!categories || categories.length === 0) {
+                return {
+                    message: "No categories found.",
+                    data: []
+                };
+            }
+            // Case 2: Successfully found categories
+            return {
+                ...CATEGORIES_RETRIEVED,
+                data: categories
+            };
+        } catch (error) {
+            // Case 3: Handle unexpected errors
+            throw new HttpException(
+                'An error occurred while retrieving categories.',
+                STATUS_INTERNAL_SERVER_ERROR
+            );
         }
     }
 
