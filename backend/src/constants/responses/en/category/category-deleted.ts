1085d65979e3c19097fb1e867610377a8350af2b
HawraaYounes <hawraayounes0@gmail.com>
2024-11-05 18:15:20 +0200

    added delete category response

---
+++ backend/src/constants/responses/en/category/category-deleted.ts
@@ -0,0 +1,8 @@
+import { STATUS_OK } from "src/constants/codes/status-codes";
+
+export const CATEGORY_DELETED = {
+    success: true,
+    statusCode: STATUS_OK,
+    message: 'Category deleted successfully',
+    data: null,
+  };
