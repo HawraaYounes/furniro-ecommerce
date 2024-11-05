2225a68bea4ccaad3f1b4aa43a16fcb959c95d1b
HawraaYounes <hawraayounes0@gmail.com>
2024-11-05 19:40:01 +0200

    added api operations for user controller

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -12,37 +12,41 @@ import {
 import { UsersService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiTags, ApiOperation } from '@nestjs/swagger';
 
 @ApiTags('user')
 @Controller('user')
 @UseInterceptors(ClassSerializerInterceptor)
 export class UserController {
-  constructor(private readonly userService: UsersService) { }
+  constructor(private readonly userService: UsersService) {}
 
   @Post()
   @ApiBody({ type: CreateUserDto })
+  @ApiOperation({ summary: 'Create a new user' })
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.userService.createUser(createUserDto);
   }
 
   @Get()
-  // @Roles(Role.Admin)
+  @ApiOperation({ summary: 'Retrieve all users' })
   findAll() {
     return this.userService.findAllUser();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Retrieve a specific user by ID' })
   findOne(@Param('id') id: string) {
     return this.userService.viewUser(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a specific user by ID' })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.updateUser(+id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a specific user by ID' })
   remove(@Param('id') id: string) {
     return this.userService.removeUser(+id);
   }
