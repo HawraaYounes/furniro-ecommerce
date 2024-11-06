de052fe7007ff4871265ba200ee8afcc54e4502f
HawraaYounes <hawraayounes0@gmail.com>
2024-11-06 12:53:28 +0200

    added product not found response

---
+++ backend/src/constants/responses/en/product/product-not-found.ts
@@ -0,0 +1,8 @@
+import { STATUS_NOT_FOUND } from "src/constants/codes/status-codes";
+
+export const PRODUCT_NOT_FOUND = {
+    success: false,
+    statusCode: STATUS_NOT_FOUND,
+    message: 'Product not found',
+    data: null,
+  };
