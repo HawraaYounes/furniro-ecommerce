92203112518d6d462f96d356b5a6be4e311148c1
HawraaYounes <hawraayounes0@gmail.com>
2024-11-06 12:57:27 +0200

    added no products found response

---
+++ backend/src/constants/responses/en/product/no-products-found.ts
@@ -0,0 +1,8 @@
+import { STATUS_NO_CONTENT } from "src/constants/codes/status-codes";
+
+export const NO_PRODUCTS_FOUND = {
+    success: true,
+    statusCode: STATUS_NO_CONTENT,
+    message: 'No Products found',
+    data: [],
+  };
