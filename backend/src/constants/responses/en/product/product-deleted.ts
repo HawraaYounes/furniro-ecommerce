19ff70912c58f1a77c4e5e9ee70866596a1ce658
HawraaYounes <hawraayounes0@gmail.com>
2024-11-06 12:53:07 +0200

    added product deleted response

---
+++ backend/src/constants/responses/en/product/product-deleted.ts
@@ -0,0 +1,8 @@
+import { STATUS_OK } from "src/constants/codes/status-codes";
+
+export const PRODUCT_DELETED = {
+    success: true,
+    statusCode: STATUS_OK,
+    message: 'Product successfully deleted',
+    data: null,
+  };
