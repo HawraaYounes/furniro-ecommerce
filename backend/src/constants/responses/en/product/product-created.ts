15bdd67638e6744fbb14852b3207477e184096ad
HawraaYounes <hawraayounes0@gmail.com>
2024-11-06 12:50:20 +0200

    added product created response

---
+++ backend/src/constants/responses/en/product/product-created.ts
@@ -0,0 +1,8 @@
+import { STATUS_CREATED } from "src/constants/codes/status-codes";
+
+export const PRODUCT_CREATED = {
+    success: true,
+    statusCode: STATUS_CREATED,
+    message: 'Product successfully created',
+    data: null,
+  };
