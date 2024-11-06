00b20dd691593b85d4bd86a1659996df69535af8
HawraaYounes <hawraayounes0@gmail.com>
2024-11-06 12:53:16 +0200

    added product found response

---
+++ backend/src/constants/responses/en/product/product-found.ts
@@ -0,0 +1,8 @@
+import { STATUS_OK } from "src/constants/codes/status-codes";
+
+export const PRODUCT_FOUND = {
+    success: true,
+    statusCode: STATUS_OK,
+    message: 'Product found successfully',
+    data: null,
+  };
