b1c0ae5c6d38e956a27f0b0a5c75dcd0c8695ddb
HawraaYounes <hawraayounes0@gmail.com>
2024-11-06 12:53:40 +0200

    added product updated response

---
+++ backend/src/constants/responses/en/product/product-updated.ts
@@ -0,0 +1,8 @@
+import { STATUS_OK } from "src/constants/codes/status-codes";
+
+export const PRODUCT_UPDATED = {
+    success: true,
+    statusCode: STATUS_OK,
+    message: 'Product successfully updated',
+    data: null,
+  };
