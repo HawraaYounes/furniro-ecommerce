25070198196cfb34d71301a5a83dfc9f57534425
HawraaYounes <hawraayounes0@gmail.com>
2024-11-06 12:53:52 +0200

    added products retrieved response

---
+++ backend/src/constants/responses/en/product/products-retrieved.ts
@@ -0,0 +1,8 @@
+import { STATUS_OK } from "src/constants/codes/status-codes";
+
+export const PRODUCTS_RETRIEVED = {
+    success: true,
+    statusCode: STATUS_OK,
+    message: 'Products successfully retrieved',
+    data: [],
+  };
