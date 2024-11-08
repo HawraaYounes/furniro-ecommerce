2d3753eb61706eddc94b44f5100accdf73eed076
HawraaYounes <hawraayounes0@gmail.com>
2024-11-08 16:14:18 +0200

    provided file upload service in product module

--- backend/src/modules/product/product.module.ts
+++ backend/src/modules/product/product.module.ts
@@ -5,10 +5,11 @@ import { Category } from '../category/category.entity';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { ProductImage } from './entities/product-image.entity';
+import { FileUploadService } from '../common/file-upload.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Product, ProductImage, Category])],
+    imports: [TypeOrmModule.forFeature([Product, ProductImage, Category, FileUploadService])],
     controllers: [ProductController],
-    providers: [ProductService],
+    providers: [ProductService, FileUploadService],
 })
-export class ProductModule {}
+export class ProductModule { }
