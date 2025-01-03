cad9f8d8b8491af15b0cc222d6d430105c9a9f67
HawraaYounes <hawraayounes0@gmail.com>
2025-01-03 17:49:24 +0200

    changed gris styling

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -3,13 +3,14 @@ import { chair } from "../assets";
 import styles from "../style";
 
 const ProductCard = ({ product }) => {
- // console.log("PRODUCT", product);
+ console.log("PRODUCT", product);
   return (
     <div className="overflow-hidden shadow-md hover:shadow-lg bg-[#F4F5F7] text-left h-[450px]">
       <div
         className="img-div h-2/3 w-full bg-cover bg-center"
-        style={{ backgroundImage: `url(${chair})` }} 
+        style={{ backgroundImage: `url(${product.images[0]})` }} 
       />
+      <img src={product.images[0]}/>
       <div className="pt-4 pl-4 pb-7">
         <h3 className="font-semibold font-poppins text-lg text-gray-800">
           {product.name }  
