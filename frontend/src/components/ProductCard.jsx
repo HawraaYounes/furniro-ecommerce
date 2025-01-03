d3e3b6af7ee049dceab83e6532b1b91b74068172
HawraaYounes <hawraayounes0@gmail.com>
2025-01-03 20:07:26 +0200

    styles product card

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -3,21 +3,22 @@ import { chair } from "../assets";
 import styles from "../style";
 
 const ProductCard = ({ product }) => {
- console.log("PRODUCT", product);
+ console.log("PRODUCT URL", product.images);
+ console.log("-------------------------------")
   return (
-    <div className="overflow-hidden shadow-md hover:shadow-lg bg-[#F4F5F7] text-left h-[450px]">
+    <div className="overflow-hidden shadow-md hover:shadow-lg bg-[#F4F5F7] text-left h-[400px] mb-5">
       <div
         className="img-div h-2/3 w-full bg-cover bg-center"
-        style={{ backgroundImage: `url(${product.images[0]})` }} 
+        style={{ backgroundImage: `url(${product.images[0].url})` }} 
       />
-      <img src={product.images[0]}/>
       <div className="pt-4 pl-4 pb-7">
         <h3 className="font-semibold font-poppins text-lg text-gray-800">
           {product.name }  
         </h3>
         <p className={`${styles.paragraph}`}>{product.title}</p>
+        <p className={`font-poppins font-medium truncate text-graydarker`}>{product.description}</p>
         <p className="text-gray-700 font-semibold mt-2">
-          Rp {product.price.toLocaleString()}
+          $ {product.price.toLocaleString()}
         </p>
       </div>
     </div>
