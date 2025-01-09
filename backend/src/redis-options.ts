d2c726ced90320c292ff62d3de51567ea1e46168
HawraaYounes <hawraayounes0@gmail.com>
2025-01-09 07:10:28 +0200

    got redis credentials from env

--- backend/src/redis-options.ts
+++ backend/src/redis-options.ts
@@ -1,13 +1,15 @@
 import { CacheModuleAsyncOptions } from '@nestjs/cache-manager';
+import { ConfigService } from '@nestjs/config';
 import { redisStore } from 'cache-manager-redis-store';
 
 export const RedisOptions: CacheModuleAsyncOptions = {
   isGlobal: true,
-  useFactory: async () => {
+  inject: [ConfigService],
+  useFactory: async (configService: ConfigService) => {
     const store = await redisStore({
       socket: {
-        host: '127.0.0.1',
-        port: 6379,
+        host: configService.get('REDIS_HOST'),
+        port: parseInt(configService.get('REDIS_PORT')),
       },
     });
     return {
