1bf732dce88692c72d0140b3c39f9f0733a5421e
HawraaYounes <hawraayounes0@gmail.com>
2025-01-12 11:26:35 +0200

    added get all collors service method

--- backend/src/modules/product/services/color.service.ts
+++ backend/src/modules/product/services/color.service.ts
@@ -7,6 +7,7 @@ import { CommonResponses } from 'src/constants/responses/en/common-responses';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 import { ColorResponses } from 'src/constants/responses/en/color-responses';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ColorService {
@@ -14,7 +15,8 @@ export class ColorService {
         @InjectRepository(Color)
         private colorRepository: Repository<Color>,
         @Inject(CACHE_MANAGER) private cacheManager: Cache,
-    ) {}
+        private configService: ConfigService
+    ) { }
 
     async create(payload: CreateColorDto) {
         try {
@@ -41,5 +43,33 @@ export class ColorService {
             return CommonResponses.INTERNAL_SERVER_ERROR;
         }
     }
+
+    async findAll() {
+        try {
+            // Check if colors are cached
+            const cachedColors = await this.cacheManager.get('colors');
+            if (cachedColors) {
+                return {
+                    ...ColorResponses.COLORS_RETRIEVED,
+                    data: cachedColors,
+                };
+            }
+
+            // Fetch colors from the database
+            const colors = await this.colorRepository.find();
+            if (!colors || colors.length === 0) {
+                return ColorResponses.NO_COLORS_FOUND;
+            }
+
+            await this.cacheManager.set('colors', colors, this.configService.get<number>('CACHE_TTL'));
+            return {
+                ...ColorResponses.COLORS_RETRIEVED,
+                data: colors,
+            };
+        } catch (error) {
+            return CommonResponses.INTERNAL_SERVER_ERROR;
+        }
+    }
+
 
 }
