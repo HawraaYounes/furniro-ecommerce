4a0fe6aa724e802ff370740884ae08485d49d91e
HawraaYounes <hawraayounes0@gmail.com>
2025-01-14 21:09:58 +0200

    added colors div

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -34,7 +34,7 @@ const ProductDetails = () => {
             className={`sm:w-[76px] sm:h-20 m-3  sm:mb-3 w-32 h-32 object-cover cursor-pointer ${
               currentImage === image.url ? "border-2 border-black p-[2px]" : ""
             }`}
-            onClick={() => setCurrentImage(image.url)} 
+            onClick={() => setCurrentImage(image.url)}
           />
         ))}
       </div>
@@ -46,9 +46,26 @@ const ProductDetails = () => {
         />
       </div>
       <div className="sm:w-5/12 order-3 flex flex-col font-semibold text-left">
-       <p className={`font-poppins text-4xl text-wrap mb-2`}>{product.name}</p>
-       <p className="text-gray font-medium text-[24px] mb-2">$ {product.price}</p>
-       <p className="font-normal text-sm font-poppins sm:w-3/4 sm:text-wrap">{product.description}</p>
+        <p className={`font-poppins text-4xl text-wrap mb-2`}>{product.name}</p>
+        <p className="text-gray font-medium text-[24px] mb-2">
+          $ {product.price}
+        </p>
+        <p className="font-normal text-sm font-poppins sm:w-3/4 sm:text-wrap">
+          {product.description}
+        </p>
+        <div className="colors-div mt-6">
+          <p className="text-sm font-normal text-gray mb-2 font-poppins">Color</p>
+          <div className="flex gap-2">
+            {/* Static colors for design */}
+            {["#d4bf96", "#15667B", "#000000"].map((color, index) => (
+              <div
+                key={index}
+                className="w-7 h-7 rounded-full cursor-pointer"
+                style={{ backgroundColor: color }}
+              ></div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
