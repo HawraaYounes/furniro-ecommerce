2a582be6d50f65cf234da34bac5dc3a9e1cfe274
HawraaYounes <hawraayounes0@gmail.com>
2025-01-17 07:47:04 +0200

    added IncrementDecrementButton

---
+++ frontend/src/components/IncrementDecrementButton.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const IncrementDecrementButton = ({ maxValue = 100 }) => {
+  const [count, setCount] = useState(1);
+
+  const buttonStyle =
+    "font-poppins text-black py-3 px-8 rounded-none flex items-center justify-center font-normal";
+
+  return (
+    <div className="flex border border-black rounded-lg items-center overflow-hidden">
+      <button
+        className={`${buttonStyle} bg-white`}
+        onClick={() => setCount((prev) => Math.max(prev - 1, 1))}
+      >
+        -
+      </button>
+      <p className="font-poppins text-sm px-4">{count}</p>
+      <button
+        className={`${buttonStyle} bg-white`}
+        onClick={() => setCount((prev) => Math.min(prev + 1, maxValue))}
+      >
+        +
+      </button>
+    </div>
+  );
+};
+
+export default IncrementDecrementButton;
