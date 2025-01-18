3537a9f82cf5ecdbb99cbf6742a914760b90cd15
HawraaYounes <hawraayounes0@gmail.com>
2025-01-18 13:51:20 +0200

    made button and increment have same sizes

--- frontend/src/components/IncrementDecrementButton.jsx
+++ frontend/src/components/IncrementDecrementButton.jsx
@@ -4,7 +4,7 @@ const IncrementDecrementButton = ({ maxValue = 100 }) => {
   const [count, setCount] = useState(1);
 
   const buttonStyle =
-    "font-poppins text-black py-3 px-8 rounded-none flex items-center justify-center font-normal";
+    "font-poppins text-black py-3 px-8 md:py-[15px] md:px-[50px] rounded-none flex items-center justify-center font-normal";
 
   return (
     <div className="flex border border-black rounded-lg items-center overflow-hidden">
@@ -14,7 +14,7 @@ const IncrementDecrementButton = ({ maxValue = 100 }) => {
       >
         -
       </button>
-      <p className="font-poppins text-sm px-4">{count}</p>
+      <p className="font-poppins px-4">{count}</p>
       <button
         className={`${buttonStyle} bg-white`}
         onClick={() => setCount((prev) => Math.min(prev + 1, maxValue))}
