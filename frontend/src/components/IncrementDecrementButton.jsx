adef25ff37d6841da31aae8f1e8c440a52d7a436
HawraaYounes <hawraayounes0@gmail.com>
2025-01-18 14:03:08 +0200

    updated increment button style

--- frontend/src/components/IncrementDecrementButton.jsx
+++ frontend/src/components/IncrementDecrementButton.jsx
@@ -3,20 +3,25 @@ import { useState } from "react";
 const IncrementDecrementButton = ({ maxValue = 100 }) => {
   const [count, setCount] = useState(1);
 
+  // Outer container style matches the Button component's padding
+  const containerStyle =
+    "font-poppins bg-white text-black border border-black p-3 md:py-[15px] rounded-lg flex justify-between font-normal";
+
+  // Inner button styles
   const buttonStyle =
-    "font-poppins text-black py-3 px-8 md:py-[15px] md:px-[50px] rounded-none flex items-center justify-center font-normal";
+    "text-black bg-white flex items-center justify-center font-normal ";
 
   return (
-    <div className="flex border border-black rounded-lg items-center overflow-hidden">
+    <div className={`${containerStyle}`}>
       <button
-        className={`${buttonStyle} bg-white`}
+        className={`${buttonStyle}`}
         onClick={() => setCount((prev) => Math.max(prev - 1, 1))}
       >
         -
       </button>
-      <p className="font-poppins px-4">{count}</p>
+      <p className="font-poppins px-8">{count}</p>
       <button
-        className={`${buttonStyle} bg-white`}
+        className={`${buttonStyle}`}
         onClick={() => setCount((prev) => Math.min(prev + 1, maxValue))}
       >
         +
