e019b8a893c7f5c0a3aaa4ad80d536f67b7506d3
HawraaYounes <hawraayounes0@gmail.com>
2025-01-19 18:58:36 +0200

    added add product color controller

--- backend/src/modules/product/controllers/product.controller.ts
+++ backend/src/modules/product/controllers/product.controller.ts
@@ -9,68 +9,82 @@ import { FindProductParamsDto } from '../dto/find-product.dto';
 import { UpdateProductDto } from '../dto/update-product.dto';
 import { DeleteProductParamsDto } from '../dto/delete-product.dto';
 import { Public } from 'src/modules/auth/public-strategy';
+import { AddProductColorBodyDto, AddProductColorParamsDto } from 'src/modules/category/dto/add-product-color.dto';
 
 @Controller('products')
 export class ProductController {
-    constructor(private readonly productService: ProductService) { }
+  constructor(private readonly productService: ProductService) { }
 
-    @Post()
-    @UseInterceptors(
-      FilesInterceptor('images', 10, {
-        storage: diskStorage({
-          destination: './uploads/products',
-          filename: (req, file, callback) => {
-            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-            const ext = extname(file.originalname);
-            callback(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
-          },
-        }),
+  @Post()
+  @UseInterceptors(
+    FilesInterceptor('images', 10, {
+      storage: diskStorage({
+        destination: './uploads/products',
+        filename: (req, file, callback) => {
+          const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          callback(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+        },
       }),
-    )
-    async createProduct(
-      @Body() createProductDto: CreateProductDto,
-      @UploadedFiles() files: Express.Multer.File[],
-    ) {
-      if (files.length === 0) {
-        throw new Error('At least one image must be uploaded.');
-      }
-      return this.productService.createProduct(createProductDto, files);
+    }),
+  )
+  async createProduct(
+    @Body() createProductDto: CreateProductDto,
+    @UploadedFiles() files: Express.Multer.File[],
+  ) {
+    if (files.length === 0) {
+      throw new Error('At least one image must be uploaded.');
     }
-    
-    @Public()
-    @Get()
-    async findAll(@Query() paginationDto: PaginationDto) {
-      return this.productService.findAll(paginationDto);
-    }
+    return this.productService.createProduct(createProductDto, files);
+  }
 
-    @Public()
-    @Get(':id')
-    async findOne(@Param() params: FindProductParamsDto) {
-        const response = await this.productService.findOne(params);
-        return response;
-    }
+  @Public()
+  @Get()
+  async findAll(@Query() paginationDto: PaginationDto) {
+    return this.productService.findAll(paginationDto);
+  }
 
-    @Put(':id')
-    async update(
-        @Param('id') id: number,
-        @Body() payload: UpdateProductDto,
-    ) {
-        const response = await this.productService.update(id, payload);
-        return response;
-    }
+  @Public()
+  @Get(':id')
+  async findOne(@Param() params: FindProductParamsDto) {
+    const response = await this.productService.findOne(params);
+    return response;
+  }
 
-    @Delete(':id')
-    async delete(@Param() params: DeleteProductParamsDto) {
-        const response = await this.productService.delete(params);
-        return response;
-    }
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() payload: UpdateProductDto,
+  ) {
+    const response = await this.productService.update(id, payload);
+    return response;
+  }
 
-    @Post(':productId/images')
-    async addImage(
-        @Param('productId') productId: number,
-        @Body('url') url: string,
-    ) {
-        const response = await this.productService.addImage(productId, url);
-        return response;
-    }
+  @Delete(':id')
+  async delete(@Param() params: DeleteProductParamsDto) {
+    const response = await this.productService.delete(params);
+    return response;
+  }
+
+  @Post(':productId/images')
+  async addImage(
+    @Param('productId') productId: number,
+    @Body('url') url: string,
+  ) {
+    const response = await this.productService.addImage(productId, url);
+    return response;
+  }
+
+  @Post(':id/new-color')
+  async addProductColor(@Param() params:AddProductColorParamsDto, @Body() body: AddProductColorBodyDto) {
+    const response = await this.productService.addProductColor(params, body);
+    return response;
+  }
+
+  //TODO: move this route for seperate controller
+  @Get('/clear/cache')
+  @Public()
+  async clearCache() {
+    return await this.productService.clearCache();
+  }
 }
