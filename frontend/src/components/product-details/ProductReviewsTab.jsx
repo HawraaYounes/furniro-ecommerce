aae608aac22743f8db06f58a27cf0f389d81ef01
HawraaYounes <hawraayounes0@gmail.com>
2025-02-04 16:48:17 +0200

    implemented product review section with basic styling

--- frontend/src/components/product-details/ProductReviewsTab.jsx
+++ frontend/src/components/product-details/ProductReviewsTab.jsx
@@ -1,10 +1,59 @@
+import StarRating from "./StarRating";
 
 const ProductReviewsTab = () => {
+  const averageRating = 4.9;
+  const totalReviews = 214;
+  const reviews = [
+    {
+      name: "Isabella P.",
+      rating: 5,
+      comment:
+        "Absolutely love this sunscreen! It feels lightweight, blends seamlessly into my skin without any white cast, and leaves a soft, dewy finish. Plus, knowing it’s reef-safe and packed with nourishing ingredients makes it a must-have in my daily routine. Highly recommend!",
+    },
+    {
+      name: "Liam S.",
+      rating: 3.5,
+      comment:
+        "I’ve been searching for the perfect moisturizer, and I finally found it! This product is so hydrating, absorbs quickly, and doesn’t leave a greasy residue. The natural ingredients make my skin feel revitalized and refreshed. It’s definitely a new staple in my skincare regimen.",
+    },
+    {
+      name: "Olivia M.",
+      rating: 1,
+      comment: "Bad product! Dont Buy it!",
+    },
+  ];
   return (
-    <div>
-      REVIEWS TAB COMPONENT
-    </div>
-  )
-}
+    <div className="flex flex-wrap sm:flex-nowrap w-full font-poppins gap-6">
+      {/* Average Rating */}
+      <div className="flex items-center p-4 border-b border-gray w-1/4 justify-between ">
+        <div>
+          <p className="text-3xl font-bold">
+            {averageRating.toFixed(1)}
+            <span className="text-xs font-light">/ 5</span>
+          </p>
+        </div>
+        <div className="flex flex-col">
+          <StarRating averageRating={averageRating} />
+          <span className="text-gray text-xs mt-1">
+            Based on {totalReviews} reviews
+          </span>
+        </div>
+      </div>
 
-export default ProductReviewsTab
+      {/* Reviews List */}
+      <div className="w-3/4 text-left">
+        {reviews.map((review, index) => (
+          <div key={index} className="p-4 ">
+            <div className="flex items-center gap-2">
+              <StarRating averageRating={review.rating} />
+              <span className="font-medium">{review.name}</span>
+            </div>
+            <p className="mt-2 font-light">{review.comment}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductReviewsTab;
