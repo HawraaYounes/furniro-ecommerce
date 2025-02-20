6deb5f00ea357bf49e4538720ed0c80b31ac7f2a
HawraaYounes <hawraayounes0@gmail.com>
2025-02-20 23:26:47 +0200

    styled progress bar

--- frontend/src/components/product-details/RatingProgressBar.jsx
+++ frontend/src/components/product-details/RatingProgressBar.jsx
@@ -3,13 +3,22 @@ import { starFilledIcon } from "../../assets";
 
 const RatingProgressBar = ({ count, percentage }) => {
   return (
-    <div className="items-center flex justify-evenly w-full font-poppins text-gray font-light">
-            <span>
-              <img src={starFilledIcon}  />
-            </span>
-            <progress value={percentage} />
-            <span>{count}</span>
-          </div>
+    <div className="items-center flex justify-evenly w-full font-poppins text-gray-600 font-light text-gray gap-3">
+      <div className="flex gap-1 text-center">
+        <span>5</span>
+        <span className="self-center h-full ">
+          <img src={starFilledIcon} alt="Star Icon" className="h-full w-full" />
+        </span>
+      </div>
+      {/* Custom Progress Bar */}
+      <div className="w-full h-[6px] bg-graylight rounded-full overflow-hidden">
+        <div
+          className="h-full bg-graydarkest rounded-full"
+          style={{ width: `${percentage}%` }}
+        />
+      </div>
+      <span>{count}</span>
+    </div>
   );
 };
 
